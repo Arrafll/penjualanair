f30fa6966895688c2b1a0b4197b88bc1864196fc
Zahira Putri <zahiraputri.project@gmail.com>
2025-01-16 10:35:56 +0700

    edit footer regis

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -120,8 +120,7 @@
     <!-- end page -->
 
     <footer class="footer footer-alt">
-        2015 -
-        <script>document.write(new Date().getFullYear())</script> &copy; UBold theme by <a href="" class="text-white-50">Coderthemes</a>
+        <script>document.write(new Date().getFullYear())</script> &copy;<a href="" class="text-white-50"></a>
     </footer>
 
 @endsection
